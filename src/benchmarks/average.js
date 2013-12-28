24e25106ab9bc3cb060909fd3c769681016ca5f2
Haitao Feng <haitao.feng@intel.com>
2013-12-28 15:39:14 +0800

    Make average.js and mandelbrot.js run on IonMonkey

--- src/benchmarks/average.js
+++ src/benchmarks/average.js
@@ -17,19 +17,11 @@
   // Hook up to the harness
   benchmarks.add(new Benchmark(kernelConfig));
 
-  // Benchmark data, initialization and kernel functions
-  var a   = new Float32Array(10000);
-  var ax4 = new Float32x4Array(a.buffer);
-
   function sanityCheck() {
      return Math.abs(average(1) - simdAverage(1)) < 0.0001;
   }
 
   function initArray() {
-    var j = 0;
-    for (var i = 0, l = a.length; i < l; ++i) {
-      a[i] = 0.1;
-    }
     // Check that the two kernel functions yields the same result, roughly
     // Account for the fact that the simdAverage() is computed using float32
     // precision and the average() is using double precision
@@ -40,25 +32,32 @@
     return sanityCheck();
   }
 
+  var length = 10000;
+  var value = 0.5;
+
   function average(n) {
+    var sum = 0.0;    // Make the Ion phi node inference happy.
     for (var i = 0; i < n; ++i) {
       var sum = 0.0;
-      for (var j = 0, l = a.length; j < l; ++j) {
-        sum += a[j];
+      for (var j = 0, l = length; j < l; ++j) {
+        sum += value;
       }
     }
-    return sum/a.length;
+//    print(sum);
+    return sum/length;
   }
 
   function simdAverage(n) {
-    var ax4_length = ax4.length;
+    var sum4 = SIMD.float32x4.zero(); // Make the Ion phi node inference happy.
+    var b = SIMD.float32x4.splat(value);
     for (var i = 0; i < n; ++i) {
       var sum4 = SIMD.float32x4.zero();
-      for (var j = 0; j < ax4_length; ++j) {
-        sum4 = SIMD.float32x4.add(sum4, ax4.getAt(j));
+      for (var j = 0; j < length/4; ++j) {
+        sum4 = SIMD.float32x4.add(sum4, b);
       }
     }
-    return (sum4.x + sum4.y + sum4.z + sum4.w)/a.length;
+//    print(sum4.x + sum4.y + sum4.z + sum4.w);
+    return (sum4.x + sum4.y + sum4.z + sum4.w)/length;
   }
 
 } ());
--- src/benchmarks/run.js
+++ src/benchmarks/run.js
@@ -1,12 +1,15 @@
 if (typeof SIMD === "undefined") {
   load ('../ecmascript_simd.js');
 }
+// Float32x4Array and Int32x4Array are not supported yet in Ion.
+/*
 if (typeof Float32x4Array === "undefined") {
   load ('../float32x4array.js');
 }
 if (typeof Int32x4Array === "undefined") {
   load ('../int32x4array.js');
 }
+*/
 
 load ('base.js');
 
@@ -14,12 +17,12 @@ load ('base.js');
 load ('kernel-template.js');
 load ('average.js');
 load ('mandelbrot.js');
-load ('matrix-multiplication.js');
-load ('transform.js');
-load ('shiftrows.js');
-load ('aobench.js');
-load ('transpose4x4.js');
-load ('inverse4x4.js');
+//load ('matrix-multiplication.js');
+//load ('transform.js');
+//load ('shiftrows.js');
+//load ('aobench.js');
+//load ('transpose4x4.js');
+//load ('inverse4x4.js');
 
 function printResult (str) {
   print (str);
